3fc8e1bfeac42b590627ba235cbfb93d5f3d9b4a
eniola <opakunleeniolail2019@gmail.com>
2023-08-29 15:17:32 +0100

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,35 @@
 import React from 'react'
 import logo from "../assets/images/logo.png"
+import githubImg from '../assets/images/github.png'
+import twitterImg from '../assets/images/twitter.png'
+import youtubeImg from '../assets/images/youtube.png'
+import navigationImg from '../assets/images/navigation.png'
+import '../App.css'
 
 function Footer() {
   return (
-    <div>
-        <div className='size'>
+    <div className='footer background pt-5'>
+        <div className='size py-5'>
             <div>
-                <img src='' alt="logo" style={{width: "10rem"}} />
+                {/* <img src='' alt="logo" style={{width: "10rem"}} /> */}
             </div>
-            <div className='d-flex'>
-                <div>
-                    <img src="" alt="" />
+            <div className='d-flex social-media justify-content-center'>
+                <div className=''>
+                    <img src={twitterImg} alt="" />
                 </div>
                 <div>
-                    <img src="" alt="" />
+                    <img src={navigationImg} alt="" />
                 </div>
                 <div>
-                    <img src="" alt="" />
+                    <img src={youtubeImg} alt="" />
                 </div>
                 <div>
-                    <img src="" alt="" />
+                    <img src={githubImg} alt="" />
                 </div>
             </div>
-            <div>
-                
-            </div>
+        </div>
+        <div className='text-center attribution title w-100 mt-3 py-4'>
+            Y INNOVATION 2023 | All Rights Reserved 
         </div>
     </div>
   )
